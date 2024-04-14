11e0a5fd03c83c5826c2be2482fb926f89936354
Yousef <yousefahmedkamel7766@gmail.com>
2024-04-14 13:58:49 +0200

    Initial commit

---
+++ src/main/java/se/projects/Bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package se.projects.Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
